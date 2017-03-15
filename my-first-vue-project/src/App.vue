1b215a796117c68709dabd269ca10e1bbf471462
xiaobu <1172504039@qq.com>
2017-03-15 19:59:24 +0800

    第一次提交

--- my-first-vue-project/src/App.vue
+++ my-first-vue-project/src/App.vue
@@ -3,7 +3,7 @@
     <!--<img src="./assets/logo.png">
     <router-view></router-view>-->
     <h1 v-text="title"></h1>
-    <input v-model="newItem">
+    <input v-model="newItem" @keyup.enter="addNew">
     <ul>
       <li v-for="item in items" v-bind:class="{finished: item.isFinished}"
           v-on:click="toggleFinish(item)">
@@ -19,19 +19,20 @@
     data: function () {
       return {
         title: 'this is a todo list',
-        items: [{
-          lable: 'coding',
-          isFinished: true
-        }, {
-          lable: 'walking',
-          isFinished: true
-        }],
+        items: [],
         newItem: ''
       }
     },
     methods: {
       toggleFinish: function (item) {
         item.isFinished = !item.isFinished
+      },
+      addNew: function () {
+        this.items.push({
+          lable: this.newItem,
+          isFinished: false
+        })
+        this.newItem = ''
       }
     }
   }
