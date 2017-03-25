8483b83db1904bd23963ee12809daab2c80f184e
xiaobu <1172504039@qq.com>
2017-03-26 05:56:18 +0800

    222

---
+++ my-first-vue-project/src/components/ComponentA.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button v-on:click="onClickMe">Click!</button>
+  </div>
+</template>
+<script>
+  export default {
+    data: function () {
+      return {
+        msg: 'hello from componet a !'
+      }
+    },
+    props: ['msgfromFather'],
+    methods: {
+      onClickMe: function () {
+        console.log(this.msgfromFather)
+      }
+    }
+  }
+</script>
